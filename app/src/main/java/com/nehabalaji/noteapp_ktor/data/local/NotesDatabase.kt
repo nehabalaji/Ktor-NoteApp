f13d2b719955a4de7e6929e402b4cf416470bbec
nehabalaji <nehabalaji10@gmail.com>
2021-11-15 14:39:36 +0530

    Initial commit

---
+++ app/src/main/java/com/nehabalaji/noteapp_ktor/data/local/NotesDatabase.kt
@@ -0,0 +1,12 @@
+package com.nehabalaji.noteapp_ktor.data.local
+
+import androidx.room.Database
+import androidx.room.TypeConverters
+import com.nehabalaji.noteapp_ktor.data.local.entities.Note
+
+@Database(entities = [Note::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class NotesDatabase {
+
+    abstract fun noteDao(): NoteDao
+}
