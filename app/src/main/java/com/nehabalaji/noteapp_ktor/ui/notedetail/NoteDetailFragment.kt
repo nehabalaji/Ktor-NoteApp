10192d8dc130339a99050baa5e0079a4c1824b5a
nehabalaji <nehabalaji10@gmail.com>
2022-08-25 18:38:51 +0530

    showing formatted notes

--- app/src/main/java/com/nehabalaji/noteapp_ktor/ui/notedetail/NoteDetailFragment.kt
+++ app/src/main/java/com/nehabalaji/noteapp_ktor/ui/notedetail/NoteDetailFragment.kt
@@ -2,22 +2,50 @@ package com.nehabalaji.noteapp_ktor.ui.notedetail
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.nehabalaji.noteapp_ktor.R
+import com.nehabalaji.noteapp_ktor.data.local.entities.Note
 import com.nehabalaji.noteapp_ktor.ui.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import io.noties.markwon.Markwon
 import kotlinx.android.synthetic.main.fragment_note_detail.*
 
+@AndroidEntryPoint
 class NoteDetailFragment: BaseFragment(R.layout.fragment_note_detail) {
+
+    private val viewModel: NoteDetailViewModel by viewModels()
 
     private val args: NoteDetailFragmentArgs by navArgs()
 
+    private var curNote: Note? = null
+
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        subscribeToObservers()
         fabEditNote.setOnClickListener {
             findNavController().navigate(
                 NoteDetailFragmentDirections.actionNoteDetailFragmentToAddEditNoteFragment2(args.id)
             )
         }
+    }
+
+    private fun setMarkdownText(text: String) {
+        val markwon = Markwon.create(requireContext())
+        val markdown = markwon.toMarkdown(text)
+        markwon.setParsedMarkdown(tvNoteContent, markdown)
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.observeNoteByID(args.id).observe(viewLifecycleOwner, Observer {
+            it?.let { note ->
+                tvNoteTitle.text = note.title
+                setMarkdownText(note.content)
+                curNote = note
+            } ?: showSnackbar("Note not found")
+        })
     }
 }
--- app/src/main/java/com/nehabalaji/noteapp_ktor/ui/notedetail/NoteDetailViewModel.kt
+++ app/src/main/java/com/nehabalaji/noteapp_ktor/ui/notedetail/NoteDetailViewModel.kt
@@ -1,6 +1,12 @@
 package com.nehabalaji.noteapp_ktor.ui.notedetail
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import com.nehabalaji.noteapp_ktor.repositories.NoteRepository
 
-class NoteDetailViewModel: ViewModel() {
+class NoteDetailViewModel @ViewModelInject constructor(
+    private val repository: NoteRepository
+) : ViewModel() {
+
+    fun observeNoteByID(noteID: String) = repository.observeNoteByID(noteID)
 }
