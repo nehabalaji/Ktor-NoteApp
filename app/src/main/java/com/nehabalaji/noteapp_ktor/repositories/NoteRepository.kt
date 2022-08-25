aa60b79afa345670577604ecd961cf948ed1b02f
nehabalaji <nehabalaji10@gmail.com>
2022-08-25 17:36:06 +0530

    Implementing the caching mechanism

--- app/src/main/java/com/nehabalaji/noteapp_ktor/repositories/NoteRepository.kt
+++ app/src/main/java/com/nehabalaji/noteapp_ktor/repositories/NoteRepository.kt
@@ -2,10 +2,14 @@ package com.nehabalaji.noteapp_ktor.repositories
 
 import android.app.Application
 import com.nehabalaji.noteapp_ktor.data.local.NoteDao
+import com.nehabalaji.noteapp_ktor.data.local.entities.Note
 import com.nehabalaji.noteapp_ktor.data.remote.NoteApi
 import com.nehabalaji.noteapp_ktor.data.remote.requests.AccountRequest
 import com.nehabalaji.noteapp_ktor.other.Resource
+import com.nehabalaji.noteapp_ktor.other.checkForInternetConnection
+import com.nehabalaji.noteapp_ktor.other.networkBoundResource
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -14,6 +18,25 @@ class NoteRepository @Inject constructor(
     private val noteApi: NoteApi,
     private val context: Application
 ) {
+
+    fun getAllNotes(): Flow<Resource<List<Note>>> {
+        return networkBoundResource(
+            query = {
+                noteDao.getAllNotes()
+            },
+            fetch = {
+                noteApi.getNotes()
+            },
+            saveFetchResult = { response ->
+                response.body()?.let {
+                    // TODO: insert notes in database
+                }
+            },
+            shouldFetch = {
+                checkForInternetConnection(context)
+            }
+        )
+    }
     suspend fun login(email: String, password: String) = withContext(Dispatchers.IO) {
         try {
             val response = noteApi.login(AccountRequest(email, password))
