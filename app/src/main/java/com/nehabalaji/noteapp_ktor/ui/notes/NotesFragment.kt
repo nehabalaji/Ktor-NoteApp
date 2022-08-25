f1df70f9283e6be17cef65ab6176c8bde0b1f97f
nehabalaji <nehabalaji10@gmail.com>
2022-08-25 17:49:34 +0530

    showing notes in Note Fragment

--- app/src/main/java/com/nehabalaji/noteapp_ktor/ui/notes/NotesFragment.kt
+++ app/src/main/java/com/nehabalaji/noteapp_ktor/ui/notes/NotesFragment.kt
@@ -1,15 +1,21 @@
 package com.nehabalaji.noteapp_ktor.ui.notes
 
 import android.content.SharedPreferences
+import android.content.pm.ActivityInfo.SCREEN_ORIENTATION_USER
 import android.os.Bundle
 import android.view.*
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nehabalaji.noteapp_ktor.R
+import com.nehabalaji.noteapp_ktor.adapters.NoteAdapter
 import com.nehabalaji.noteapp_ktor.other.Constants.KEY_LOGGED_IN_EMAIL
 import com.nehabalaji.noteapp_ktor.other.Constants.KEY_PASSWORD
 import com.nehabalaji.noteapp_ktor.other.Constants.NO_EMAIL
 import com.nehabalaji.noteapp_ktor.other.Constants.NO_PASSWORD
+import com.nehabalaji.noteapp_ktor.other.Status
 import com.nehabalaji.noteapp_ktor.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_notes.*
@@ -17,6 +23,10 @@ import javax.inject.Inject
 
 @AndroidEntryPoint
 class NotesFragment : BaseFragment(R.layout.fragment_notes) {
+
+    private lateinit var noteAdapter: NoteAdapter
+
+    private val viewModel: NotesViewModel by viewModels()
 
     @Inject
     lateinit var sharedPref: SharedPreferences
@@ -32,9 +42,48 @@ class NotesFragment : BaseFragment(R.layout.fragment_notes) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().requestedOrientation = SCREEN_ORIENTATION_USER
+        setupRecyclerView()
+        subscribeToObservers()
+        noteAdapter.setOnItemClickListener {
+            findNavController().navigate(
+                NotesFragmentDirections.actionNotesFragmentToNoteDetailFragment(it.id)
+            )
+        }
         fabAddNote.setOnClickListener {
             findNavController().navigate(NotesFragmentDirections.actionNotesFragmentToAddEditNoteFragment(""))
         }
+    }
+
+    private fun subscribeToObservers() {
+        viewModel.allNotes.observe(viewLifecycleOwner, Observer {
+            it?.let { event ->
+                val result = event.peekContent()
+                when(result.status) {
+                    Status.SUCCESS -> {
+                        noteAdapter.notes = result.data!!
+                        swipeRefreshLayout.isRefreshing = false
+                    }
+                    Status.ERROR -> {
+                        event.getContentIfNotHandled()?.let { errorResource ->
+                            errorResource.message?.let { message ->
+                                showSnackbar(message)
+                            }
+                        }
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+                        swipeRefreshLayout.isRefreshing = false
+                    }
+                    Status.LOADING -> {
+                        result.data?.let { notes ->
+                            noteAdapter.notes = notes
+                        }
+                        swipeRefreshLayout.isRefreshing = true
+                    }
+                }
+            }
+        })
     }
 
     private fun logout() {
@@ -48,6 +97,13 @@ class NotesFragment : BaseFragment(R.layout.fragment_notes) {
             navOptions
         )
     }
+
+    private fun setupRecyclerView() = rvNotes.apply {
+        noteAdapter = NoteAdapter()
+        adapter = noteAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when(item.itemId) {
--- app/src/main/java/com/nehabalaji/noteapp_ktor/ui/notes/NotesViewModel.kt
+++ app/src/main/java/com/nehabalaji/noteapp_ktor/ui/notes/NotesViewModel.kt
@@ -1,6 +1,24 @@
 package com.nehabalaji.noteapp_ktor.ui.notes
 
-import androidx.lifecycle.ViewModel
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.*
+import com.nehabalaji.noteapp_ktor.data.local.entities.Note
+import com.nehabalaji.noteapp_ktor.other.Event
+import com.nehabalaji.noteapp_ktor.other.Resource
+import com.nehabalaji.noteapp_ktor.repositories.NoteRepository
 
-class NotesViewModel: ViewModel() {
+class NotesViewModel @ViewModelInject constructor(
+    private val repository: NoteRepository
+) : ViewModel() {
+
+    private val _forceUpdate = MutableLiveData<Boolean>(false)
+
+    private val _allNotes = _forceUpdate.switchMap {
+        repository.getAllNotes().asLiveData(viewModelScope.coroutineContext)
+    }.switchMap {
+        MutableLiveData(Event(it))
+    }
+    val allNotes: LiveData<Event<Resource<List<Note>>>> = _allNotes
+
+    fun syncAllNotes() = _forceUpdate.postValue(true)
 }
