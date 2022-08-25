5e5d1c87dfee68a6744e8c15c513f19d89829c48
nehabalaji <nehabalaji10@gmail.com>
2022-08-25 17:22:24 +0530

    Logging in users

--- app/src/main/java/com/nehabalaji/noteapp_ktor/other/Constants.kt
+++ app/src/main/java/com/nehabalaji/noteapp_ktor/other/Constants.kt
@@ -4,6 +4,10 @@ object Constants {
 
     const val DATABASE_NAME = "notes_db"
     const val BASE_URL = "http://192.168.0.106:8001"
+
+    const val KEY_LOGGED_IN_EMAIL = "KEY_LOGGED_IN_EMAIL"
+    const val KEY_PASSWORD = "KEY_PASSWORD"
+
     const val ENCRYPTED_SHARED_PREFERENCE_NAME = "enc_shared_pref"
     val IGNORE_AUTH_URLS = listOf("/login", "/register")
 }
--- app/src/main/java/com/nehabalaji/noteapp_ktor/ui/MainActivity.kt
+++ app/src/main/java/com/nehabalaji/noteapp_ktor/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.nehabalaji.noteapp_ktor.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.nehabalaji.noteapp_ktor.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
